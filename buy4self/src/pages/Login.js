d18e1409e6904a725b9154f59e8c6479d760df87
dezeve <dezeve@outlook.com>
2023-10-13 15:00:58 +0300

    Add login and forgotpassword

--- buy4self/src/pages/Login.js
+++ buy4self/src/pages/Login.js
@@ -3,9 +3,49 @@ import Layout from '../layout/Layout';
 
 const Login = () => {
     return <Layout>
-        <div className='card'></div>
+        <section class="vh-80">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow-2-strong">
+                    <div class="card-body p-5 text-center">
 
+                        <h3 class="mb-5">Login</h3>
+                        
+                        <label class="form-label">Email</label>
+                        <div class="form-outline mb-4">
+                        <input type="email" class="form-control form-control-lg" />
+                        
+                        </div>
 
+                        <label class="form-label">Password</label>
+                        <div class="form-outline mb-4">
+                        <input type="password" class="form-control form-control-lg" />
+                        </div>
+
+                        <div class="row mb-4">
+                        <div class="col d-flex justify-content-center">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox"/>
+                            <label class="form-check-label"> Remember password </label>
+                        </div>
+                        </div>
+
+                        <div class="col">
+                        <a href="#!">Forgot password?</a>
+                        </div>
+                    </div>
+                    
+                    <br />
+
+                    <button class="btn btn-primary btn-lg btn-block" type="submit">Submit</button>
+
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </section>
     </Layout>;
 };
 
