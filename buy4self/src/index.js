5d8298b8664e70eb2320bfeddc9081ff1066f41a
dezeve <dezeve@outlook.com>
2023-10-16 16:36:46 +0300

    Add Redux

--- buy4self/src/index.js
+++ buy4self/src/index.js
@@ -6,10 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import "./custom-bootstrap.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 const root = ReactDOM.createRoot(document.getElementById('root'));
+import store from './redux/store';
+import { Provider } from 'react-redux';
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
