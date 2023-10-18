90cc98ddb8501c21b8b37416db7ccc4f6d5a0abf
dezeve <dezeve@outlook.com>
2023-10-18 15:51:45 +0300

    Fix offers

--- buy4self/src/pages/Offers.js
+++ buy4self/src/pages/Offers.js
@@ -6,49 +6,49 @@ const Offers = () => {
     <Layout>
 
         <div class="p-5 bg-primary bg-gradient text-white rounded">
-            <h1 class="mb-4">Flash Sales</h1>
             <div class="row">
                 <div class="col-sm">
+                    <h1 class="mb-4">Flash Sales</h1>
                     Integer iaculis sapien quis velit ultricies blandit. Nullam pellentesque porttitor turpis eget sodales. Vivamus eros ligula, convallis at aliquam et, finibus at mi. Phasellus accumsan semper quam eget convallis. Morbi non sem tempus, mollis nibh a, molestie nunc. Nullam ultricies magna ligula, ac tempor magna commodo id. Morbi eget facilisis nulla. Nullam aliquam dui eget neque cursus, volutpat blandit orci pulvinar. Duis eget ullamcorper mauris, vitae luctus massa. Mauris facilisis mi nec augue vulputate, in efficitur purus hendrerit. Mauris auctor lorem vitae leo interdum, vel tincidunt est ultricies. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In hac habitasse platea dictumst. Suspendisse potenti.
                 </div>
                 <div class="col-sm">
-                    <img style={{borderRadius:"5%"}} src="https://images.unsplash.com/photo-1583573636246-18cb2246697f?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1938" alt="sale" width="700" height="250" />
+                    <img style={{borderRadius:"5%"}} src="https://images.unsplash.com/photo-1583573636246-18cb2246697f?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1938" alt="sale" width="700" height="300" />
                 </div>
             </div>
         </div>
 
         <div class="mt-4 p-5 bg-dark bg-gradient text-white rounded">
-            <h1 class="mb-4">10% Off</h1>
             <div class="row">
                 <div class="col-sm">
-                Aliquam id bibendum ante. Sed non faucibus mauris. Duis ut sapien finibus, aliquam justo nec, tempus tortor. Praesent ut ante vitae ex malesuada suscipit non sed tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur feugiat lobortis nibh ac finibus. Sed eget erat eget urna mattis pellentesque a sed ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris tempor diam felis, id lacinia ante tincidunt a. Sed cursus lacus a risus vestibulum, ac mattis massa dictum. Aliquam egestas venenatis vehicula.
+                    <h1 class="mb-4">10% Off</h1>
+                    Aliquam id bibendum ante. Sed non faucibus mauris. Duis ut sapien finibus, aliquam justo nec, tempus tortor. Praesent ut ante vitae ex malesuada suscipit non sed tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur feugiat lobortis nibh ac finibus. Sed eget erat eget urna mattis pellentesque a sed ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris tempor diam felis, id lacinia ante tincidunt a. Sed cursus lacus a risus vestibulum, ac mattis massa dictum. Aliquam egestas venenatis vehicula.
                 </div>
                 <div class="col-sm">
-                <img style={{borderRadius:"5%"}} src="https://images.unsplash.com/photo-1506617420156-8e4536971650?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2023" alt="sale" width="700" height="250" />
+                <img style={{borderRadius:"5%"}} src="https://images.unsplash.com/photo-1506617420156-8e4536971650?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2023" alt="sale" width="700" height="300" />
                 </div>
             </div>
         </div>
 
         <div class="mt-4 p-5 bg-danger bg-gradient text-white rounded">
-            <h1 class="mb-4">Super Sale</h1>
             <div class="row">
                 <div class="col-sm">
+                    <h1 class="mb-4">Super Sale</h1>
                     Etiam nec ligula ut metus dignissim volutpat at sit amet elit. Vestibulum quis pellentesque arcu, vel mattis magna. Nulla sit amet magna sapien. Suspendisse eget laoreet tortor, a faucibus ipsum. Morbi vitae justo eros. Praesent consectetur, diam eget vehicula tempus, felis nunc pellentesque lacus, non molestie nisi ante a erat. Mauris ultrices orci lacinia fringilla dictum. Nulla risus metus, ultrices ac eleifend sed, pulvinar pretium magna. Morbi ut facilisis sapien. Nullam volutpat vestibulum feugiat. Nunc eros urna, auctor non ornare quis, rutrum in dolor. Donec cursus iaculis sodales. Praesent lacinia cursus porta. In hac habitasse platea dictumst. Donec vitae lorem ut urna tristique condimentum. Nulla facilisi
                 </div>
                 <div class="col-sm">
-                    <img style={{borderRadius:"5%"}} src="https://images.unsplash.com/photo-1607082350899-7e105aa886ae?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2070" alt="sale" width="700" height="250" />
+                    <img style={{borderRadius:"5%"}} src="https://images.unsplash.com/photo-1607082350899-7e105aa886ae?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2070" alt="sale" width="700" height="300" />
                 </div>
             </div>
         </div>
 
         <div class="mt-4 p-5 bg-secondary bg-gradient text-white rounded">
-            <h1 class="mb-4">Buy One Get One</h1>
             <div class="row">
                 <div class="col-sm">
+                    <h1 class="mb-4">Buy One Get One</h1>
                     Vestibulum sollicitudin elit eget enim scelerisque, et fringilla metus bibendum. Integer viverra ornare nunc a interdum. Nam eu tristique augue, eu bibendum sapien. Morbi blandit metus a nisi suscipit sagittis. Nulla fermentum dui eu felis venenatis consectetur. Sed vitae egestas justo. Aliquam erat volutpat. Aenean eleifend sed magna vestibulum elementum. Maecenas maximus, felis sit amet tristique lobortis, lectus eros porta nunc, non elementum justo tellus at mauris. Proin finibus, mi vitae aliquam ullamcorper, nisi est molestie lacus, ut fringilla elit diam vel tellus. Sed felis nulla, cursus quis pharetra at, viverra ut purus.
                 </div>
                 <div class="col-sm">
-                    <img style={{borderRadius:"5%"}} src="https://images.unsplash.com/photo-1632829882891-5047ccc421bc?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1932" alt="sale" width="700" height="250" />
+                    <img style={{borderRadius:"5%"}} src="https://images.unsplash.com/photo-1632829882891-5047ccc421bc?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1932" alt="sale" width="700" height="300" />
                 </div>
             </div>
         </div>
