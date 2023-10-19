b9f8f064b4e1e1edd4aa009bd99e410be9b2ccc6
dezeve <dezeve@outlook.com>
2023-10-19 12:33:03 +0300

    Update cart

--- buy4self/src/pages/Cart.js
+++ buy4self/src/pages/Cart.js
@@ -4,7 +4,22 @@ import Layout from "../layout/Layout";
 const Cart = () => {
     return (
         <Layout>
-            <h1>Cart</h1>
+            <div class="p-5 rounded" style={{backgroundColor:"#e6e6e6"}}>
+                <h1>Cart</h1>
+                <div className="mt-5 justify-content-center d-flex">
+                    <button type="button" class="btn btn-primary">
+                        <a href="/Payment" className="text-decoration-none text-white">
+                            Proceed To Payment
+                        </a>
+                    </button>
+                    &nbsp;
+                    <button type="button" class="btn btn-success">
+                        <a href="/" className="text-decoration-none text-white">
+                            Continue Shopping
+                        </a>
+                    </button>
+                </div>
+            </div>
         </Layout>
     );
 }
