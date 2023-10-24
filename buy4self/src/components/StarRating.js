1ff81ea422abd7065152fcdfb8ee77d704274136
dezeve <dezeve@outlook.com>
2023-10-24 16:06:41 +0300

    Add star rating component

---
+++ buy4self/src/components/StarRating.js
@@ -0,0 +1,22 @@
+import react, { useState } from "react";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const StarRating = () => {
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+    return(
+        <div className="star-rating">
+            {[...Array(5)].map((star, index) => {   
+                index += 1     
+        return (         
+          <button type="starButton" key={index} className={index <= (hover || rating) ? "on" : "off"} onClick={() => setRating(index)} onMouseEnter={() => setHover(index)} onMouseLeave={() => setHover(rating)}>
+            <span className="star"><FontAwesomeIcon icon={faStar} /></span>
+          </button>     
+        );
+        })}
+        </div>
+    );
+};
+
+export default StarRating;
--- buy4self/src/pages/Payment.js
+++ buy4self/src/pages/Payment.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../layout/Layout";
+import StarRating from "../components/StarRating";
 
 const Payment = () => {
     return (
