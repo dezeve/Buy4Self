eae3abd03f27c9f7786af1d9fe613ce99ba9afef
dezeve <dezeve@outlook.com>
2023-11-02 11:29:08 +0300

    Update payment

--- buy4self/src/index.js
+++ buy4self/src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./custom-bootstrap.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
-const root = ReactDOM.createRoot(document.getElementById('root'));
 import store from './redux/store';
 import { Provider } from 'react-redux';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
--- buy4self/src/pages/Payment.js
+++ buy4self/src/pages/Payment.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Layout from "../layout/Layout";
+import PaymentStepper from "../components/PaymentStepper";
 
 const Payment = () => {
     return (
         <Layout>
-            <h1>Payment</h1>
+            <div className="w-75 m-auto border rounded">
+                <h2 className="mt-3">Payment</h2>
+                <hr className="mb-5 mt-3" />
+                <div className="row mb-5 p-3">
+                    <div className="col-sm-6 border rounded p-3">
+                        <PaymentStepper/>
+                    </div>
+                    <div className="col-sm-6">
+                        Payment
+                    </div>
+                </div>
+            </div>
         </Layout>
     );
 }
